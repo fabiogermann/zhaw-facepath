e54af0de7c46fb511b39730a812b5f80f56d870c
Fabio Germann <fabio.germann@gmail.com>
2013-10-09 13:21:23 +0200

    INIT

---
+++ Source/FacePath/src/ch/zhaw/seps/FacePath.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ch.zhaw.seps;
+
+/**
+ * @author fabiog
+ *
+ */
+public class FacePath {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
