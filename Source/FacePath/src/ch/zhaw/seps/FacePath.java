e2812767f1e43700f51ce1cf24711012f972e8e3
cvec <c_vecsei@yahoo.de>
2013-10-09 14:58:49 +0200

    changed number

--- Source/FacePath/src/ch/zhaw/seps/FacePath.java
+++ Source/FacePath/src/ch/zhaw/seps/FacePath.java
@@ -14,7 +14,7 @@ public class FacePath {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("booooooo");
+		System.out.print("booooooo2");
 	}
 
 }
