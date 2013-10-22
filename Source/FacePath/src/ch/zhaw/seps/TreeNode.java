0fb6507df5779dabd87c637122eadd5048c5e339
Fabio Germann <fabio.germann@gmail.com>
2013-10-22 23:11:51 +0200

    added some more classes - again

--- Source/FacePath/src/ch/zhaw/seps/TreeNode.java
+++ Source/FacePath/src/ch/zhaw/seps/TreeNode.java
@@ -3,7 +3,37 @@ package ch.zhaw.seps;
 import java.util.List;
 
 public class TreeNode<T> {
-    private T data;
-    private TreeNode<T> parent;
-    private List<TreeNode<T>> children;
+	public static class Node<T> {
+        private T data;
+        private TreeNode<T> parent;
+        private List<TreeNode<T>> children;
+        
+        public void addChild(TreeNode<T> node) {
+    		// TODO Auto-generated method stub
+    	}
+        
+        public void removeChild(TreeNode<T> node) {
+    		// TODO Auto-generated method stub
+    	}
+        
+        public void getChildByKey(String key) {
+    		// TODO Auto-generated method stub
+    	}
+        
+        public void getData() {
+    		// TODO Auto-generated method stub
+    	}
+        
+        public void setData() {
+    		// TODO Auto-generated method stub
+    	}
+        
+        public void setKey() {
+    		// TODO Auto-generated method stub
+    	}
+        
+        public void getKey() {
+    		// TODO Auto-generated method stub
+    	}
+    }
 }
--- Source/FacePath/src/ch/zhaw/seps/fb/FacebookNetwork.java
+++ Source/FacePath/src/ch/zhaw/seps/fb/FacebookNetwork.java
@@ -1,8 +1,32 @@
 package ch.zhaw.seps.fb;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+
+import ch.zhaw.seps.TreeNode;
+
 public class FacebookNetwork {
+	// Datastructure for the simulation of the facebook network
 	
-	private FacebookProfile rootNode;
-	
+    private TreeNode<FacebookProfile> root;
+    private TreeNode<FacebookProfile> currentRootChild;
+    private Stack<TreeNode<FacebookProfile>> rootStack;
+    private List<TreeNode<FacebookProfile>> rootChildren;
+    private HashMap<String,FacebookProfile> rootDatacollection;
+    
+    private TreeNode<FacebookProfile> target;
+    private TreeNode<FacebookProfile> currentTargetChild;
+    private Stack<TreeNode<FacebookProfile>> targetStack;
+    private List<TreeNode<FacebookProfile>> targetChildren;
+    private HashMap<String,FacebookProfile> targetDatacollection;
+    
+    public void insertItem() {
+		// TODO Auto-generated method stub
+	}
+    
+    public void getItem() {
+		// TODO Auto-generated method stub
+	}
 
 }
